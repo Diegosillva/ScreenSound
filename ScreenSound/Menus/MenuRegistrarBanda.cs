95ce9132318e63d6c763d027b2a9d4a2cbc45e5e
Diego Silva <diego.silperes@gmail.com>
2023-10-03 19:18:57 -0400

    primeiro projeto

---
+++ ScreenSound/Menus/MenuRegistrarBanda.cs
@@ -0,0 +1,32 @@
+﻿
+using OpenAI_API;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+internal class MenuRegistrarBanda : Menu
+{
+    public override async void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        base.Executar(bandasRegistradas);
+        ExibirTituloDaOpcao("Registro das bandas");
+        Console.Write("Digite o nome da banda que deseja registrar: ");
+        string nomeDaBanda = Console.ReadLine()!;
+        Banda banda = new Banda(nomeDaBanda);
+        bandasRegistradas.Add(nomeDaBanda, banda);
+        // faz integração com o ChatGpt
+        var client = new OpenAIAPI("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");
+        var chat = client.Chat.CreateConversation();
+        chat.AppendSystemMessage($"Resuma a banda {nomeDaBanda}  em 1 parágrafo. Adote um estilo informal.");
+        string resposta = await chat.GetResponseFromChatbotAsync();
+        banda.Resumo= resposta;
+
+
+        Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+        Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+
+    }
+
+}
---
+++ ScreenSound/Menus/MenuSair.cs
@@ -0,0 +1,11 @@
+﻿using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+internal class MenuSair : Menu
+{
+    public override void Executar(Dictionary<string, Banda> bandasResgistradas)
+    {
+        Console.WriteLine("Tchau Tchau: ");
+    }
+}
